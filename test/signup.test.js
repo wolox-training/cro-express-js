7824a2dd5ededce6398362b154cceecf699de4be
Cristian Romero <c.romero.acosta@wolox.co>
2021-09-30 17:18:04 -0500

    add done into test functions

--- test/signup.test.js
+++ test/signup.test.js
@@ -9,22 +9,25 @@ const mockUser = {
 };
 
 describe('POST /users', () => {
-  test('Valid params', async () => {
+  test('Valid params', async done => {
     const response = await request(app)
       .post('/users')
       .send(mockUser);
     expect(response.statusCode).toEqual(200);
+    done();
   });
-  test('Weak password', async () => {
+  test('Weak password', async done => {
     const response = await request(app)
       .post('/users')
       .send({ ...mockUser, password: 'AB' });
     expect(response.statusCode).toEqual(422);
+    done();
   });
-  test('Email with external domain', async () => {
+  test('Email with external domain', async done => {
     const response = await request(app)
       .post('/users')
       .send({ ...mockUser, email: 'r.feynman@gmail.com' });
     expect(response.statusCode).toEqual(422);
+    done();
   });
 });
